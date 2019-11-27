8dc0ef4232a7d57d64a74cb9928d6679131afa4c
Will Hodgson <will.hodgson@current-rms.com>
2019-11-27 09:28:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,7 +49,7 @@ class SongsController < ApplicationController
 
   def upload
     CSV.foreach(params[:songs].path, headers: true) do |song|
-      Song.create()
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 
